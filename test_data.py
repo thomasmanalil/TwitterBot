09ab63f7e1e2dea16d82a984fd7a847ea6411b76
ThomasManalil <thomuuuu@gmail.com>
2020-05-09 19:39:58 +0530

    Changing class name to make it compatible with pytest

--- test_data.py
+++ test_data.py
@@ -1,20 +1,29 @@
 import unittest
 import sys
 import os
-
 import data
+ 
 
-class DataTest(unittest.TestCase): 
+class Testdata(unittest.TestCase): 
+
+    def __init__(self, *args, **kwargs):
+        super(DataTest, self).__init__(*args, **kwargs)
+        self.file = data.DATA_FILE
+
+    def tearDown(self):
+        if(os.path.exists(self.file)):
+            os.remove(self.file)
+
 
     def test_resetSinceId_noFile(self):
         data.resetSinceId()
-        self.assertFalse(os.path.exists(data.DATA_FILE))
+        self.assertFalse(os.path.exists(self.file))
 
     def test_resetSinceId_FilePresent(self):
         data.setSinceId(100)
-        self.assertTrue(os.path.exists(data.DATA_FILE))
+        self.assertTrue(os.path.exists(self.file))
         data.resetSinceId()
-        self.assertFalse(os.path.exists(data.DATA_FILE))
+        self.assertFalse(os.path.exists(self.file))
 
     def test_getSinceId_noFIle(self):
         data.resetSinceId()
@@ -22,20 +31,18 @@ class DataTest(unittest.TestCase):
     
     def test_getSinceId_EmptyFile(self):
         try:
-            emptyFile=open(data.DATA_FILE,"w")
+            emptyFile=open(self.file,"w")
             emptyFile.close()
             self.assertEqual(1,data.getSinceId())            
         except Exception as e:
             raise e
         finally:
             emptyFile.close()
-            data.resetSinceId()
-        
+                    
     def test_getSinceId_readFromFile(self):
         data.setSinceId(100)
         self.assertEqual(100, data.getSinceId())
-        data.resetSinceId()
-    
+        
 
 if __name__ == "__main__":
     unittest.main()
