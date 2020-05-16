a2f5c0be22469acd7fd12ab9fba4a7ab37757b56
ThomasManalil <thomuuuu@gmail.com>
2020-05-16 17:11:04 +0530

    Handing no file case for config json file.

--- config.py
+++ config.py
@@ -9,13 +9,22 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger()
 
 path = os.path.join(PARENT_FOLDER, "config/config.json")
-with open(path) as configFile:
-    #reading tokens from config file
-    configData=json.load(configFile)
-    CONSUMER_KEY = str(configData['CONSUMER_KEY'])
-    CONSUMER_SECRET = str(configData['CONSUMER_SECRET'])
-    ACCESS_TOKEN = str(configData['ACCESS_TOKEN'])
-    ACCESS_TOKEN_SECRET = str(configData['ACCESS_TOKEN_SECRET'])
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+try:
+    if(os.path.exists(path)):
+        with open(path) as configFile:
+            #reading tokens from config file
+            configData=json.load(configFile)
+            CONSUMER_KEY = str(configData['CONSUMER_KEY'])
+            CONSUMER_SECRET = str(configData['CONSUMER_SECRET'])
+            ACCESS_TOKEN = str(configData['ACCESS_TOKEN'])
+            ACCESS_TOKEN_SECRET = str(configData['ACCESS_TOKEN_SECRET'])
+except Exception as ex:
+    LOGGER.info(ex)
+
 
 
 
